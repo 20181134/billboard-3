64dbc16f8b627241a886e74f6b6125066c2db424
20181134 <91303066+20181134@users.noreply.github.com>
2021-12-13 19:27:14 +0900

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,10 @@
+drop database if exists login_db;
+create database login_db character set utf8 collate utf8_general_ci;
+grant all on login_db.* to 'admin'@'localhost' identified by 'password';
+use login;
+
+create table login_list (
+    username varchar(30) not null,
+    password varchar(30) not null
+)
+insert into login_list values('user', 'password');
